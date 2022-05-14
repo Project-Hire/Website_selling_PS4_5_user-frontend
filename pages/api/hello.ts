d8ab2886453e3daaef84b203835022e5f287e928
phamlam2k <mailam1309@gmail.com>
2022-05-14 22:58:43 +0700

    update husky eslint and prettier code library

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,6 +5,7 @@ type Data = {
   name: string
 }
 
+// eslint-disable-next-line require-jsdoc
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   res.status(200).json({ name: 'John Doe' })
 }
